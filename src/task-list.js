4fb3820f7242c073295744675a079b09bae7edd6
LEFTazs <leftazshun@gmail.com>
2020-01-29 18:01:36 +0100

    Make task-list dynamic - task listing, adding, modifying, deleting

---
+++ src/task-list.js
@@ -0,0 +1,357 @@
+var chosenCalendarDate = new Date(2020, 0, 1); //TODO: get this from caller
+var currentWorkday;
+
+window.onload = initalizePage;
+
+function initalizePage() {
+    currentWorkday = getWorkdayFromBackend(chosenCalendarDate);
+    console.log(currentWorkday);
+    
+    drawPage();
+    assignAllEvents();
+}
+
+function drawPage() {
+    drawTaskListTable();
+    drawStatistics();
+}
+
+function assignAllEvents() {
+    assignTaskAdderModalEvents();
+    assignTaskModifierModalEvents();
+    assignTaskDeleterModalEvents();
+    assignEditButtonEvents();
+}
+
+
+function drawTaskListTable() {
+    $('#taskList').empty();
+    
+    var allTasks = currentWorkday.tasks;
+    for (var i in allTasks) {
+        var taskId = allTasks[i].taskId;
+        var comment = allTasks[i].comment;
+        var startTime = allTasks[i].startTime;
+        var endTime = allTasks[i].endTime;
+        var length = allTasks[i].minPerTask;
+        
+        $('#taskList').append("<tr></tr>");
+        $('#taskList tr:last').append("<td>" + taskId + "</td>");
+        $('#taskList tr:last').append("<td>" + comment + "</td>");
+        $('#taskList tr:last').append("<td>" + startTime + "</td>");
+        $('#taskList tr:last').append("<td>" + endTime + "</td>");
+        $('#taskList tr:last').append("<td>" + length + "</td>");
+        $('#taskList tr:last').append('<td class="modifyButton btn-default" data-rownum="' + i + '">Modify</td>' +
+                                '<td class="deleteButton btn-default" data-rownum="' + i + '">Delete</td>');
+    }
+}
+
+function drawStatistics() {
+    var allMinutes = currentWorkday.sumPerDay;
+    $('#allMinutesStatistic').empty();
+    $('#allMinutesStatistic').append(allMinutes);
+    
+    var requiredMinutes = currentWorkday.requiredMinPerDay;
+    $('#requiredMinutesStatistic').empty();
+    $('#requiredMinutesStatistic').append(requiredMinutes);
+    
+    var extraMinutes = currentWorkday.extraMinPerDay;
+    $('#extraMinutesStatistic').empty();
+    $('#extraMinutesStatistic').append(extraMinutes);
+    
+    var className = (extraMinutes < 0) ? "negativeMinutes" : "nonNegativeMinutes";
+    $('#extraMinutesStatistic').attr("class", className);
+}
+
+
+function assignTaskAdderModalEvents() {
+    $('#modalTaskAdder').off("submit");
+    $('#modalTaskAdder').submit(function(e) {
+        e.preventDefault();
+        
+        var taskId = $('#adderTaskId').val();
+        var comment = $('#adderComment').val();
+        var startTime = $('#adderStartTime').val();
+        var endTime = $('#adderEndTime').val();
+        
+        postTaskToBackend(chosenCalendarDate, taskId, comment, startTime, endTime);
+        
+        $('#modalTaskAdder').modal('toggle');
+                
+        initalizePage();
+    });
+}
+
+function assignTaskModifierModalEvents() {
+    $('#modalTaskModifier').off("submit");
+    $('#modalTaskModifier').submit(function(e) {
+        e.preventDefault();
+        
+        var oldTaskId = $('#modifierOldTaskId').val();
+        var oldStartTime = $('#modifierOldStartTime').val();
+        var newTaskId = $('#modifierNewTaskId').val();
+        var newComment = $('#modifierNewComment').val();
+        var newStartTime = $('#modifierNewStartTime').val();
+        var newEndTime = $('#modifierNewEndTime').val();
+        
+        putModifyTaskToBackend(chosenCalendarDate, oldTaskId, oldStartTime, 
+                                newTaskId, newComment, newStartTime, newEndTime);
+        
+        $('#modalTaskModifier').modal('toggle');
+                
+        initalizePage();
+    });
+}
+
+function assignTaskDeleterModalEvents() {
+    $('#modalTaskDeleter').off("submit");
+    $('#modalTaskDeleter').submit(function(e) {
+        e.preventDefault();
+        
+        var taskId = $('#deleteTaskId').val();
+        var startTime = $('#deleteStartTime').val();
+        
+        deleteTaskFromBackend(chosenCalendarDate, taskId, startTime);
+        
+        $('#modalTaskDeleter').modal('toggle');
+                
+        initalizePage();
+    });
+}
+
+function assignEditButtonEvents() {
+    $(document).off('click', '.modifyButton');
+    $(document).on('click', '.modifyButton', function () {
+        var chosenRow = $(this).data("rownum");
+        var chosenTask = currentWorkday.tasks[chosenRow];
+        var taskId = chosenTask.taskId;
+        var startTime = removeSecondsFromTimeString(chosenTask.startTime);
+        $("#modifierOldTaskId").val(taskId);
+        $("#modifierOldStartTime").val(startTime);
+        $('#modalTaskModifier').modal('toggle');
+    });
+    
+    $(document).off('click', '.deleteButton');
+    $(document).on('click', '.deleteButton', function () {
+        var chosenRow = $(this).data("rownum");
+        var chosenTask = currentWorkday.tasks[chosenRow];
+        var taskId = chosenTask.taskId;
+        var startTime = removeSecondsFromTimeString(chosenTask.startTime);
+        $("#deleteTaskId").val(taskId);
+        $("#deleteStartTime").val(startTime);
+        $('#modalTaskDeleter').modal('toggle');
+    });
+}
+
+
+function removeSecondsFromTimeString(timeAsString) {
+    var timeParts = timeAsString.split(/:/);
+    return timeParts[0] + ":" + timeParts[1];
+}
+
+
+
+function convertStartTaskToJson(date, taskId, comment, startTime) {
+    var currentYear = date.getFullYear();
+    var currentMonth = date.getMonth() + 1;
+    var currentDay = date.getDate();
+    
+    var jsonData = JSON.stringify({ year: currentYear, 
+        month: currentMonth, 
+        day: currentDay,
+        taskId: taskId,
+        startTime: startTime,
+        comment: comment });
+    
+    return jsonData;
+}
+
+function convertFinishTaskToJson(date, taskId, startTime, endTime) {
+    var currentYear = date.getFullYear();
+    var currentMonth = date.getMonth() + 1;
+    var currentDay = date.getDate();
+    
+    var jsonData = JSON.stringify({ year: currentYear, 
+        month: currentMonth, 
+        day: currentDay,
+        taskId: taskId,
+        startTime: startTime,
+        endTime: endTime });
+    
+    return jsonData;
+}
+
+function convertModifyTaskToJson(date, taskId, startTime, newTaskId, 
+                                newComment, newStartTime, newEndTime) {
+    var currentYear = date.getFullYear();
+    var currentMonth = date.getMonth() + 1;
+    var currentDay = date.getDate();
+    
+    var jsonData = JSON.stringify({ year: currentYear, 
+        month: currentMonth, 
+        day: currentDay,
+        taskId: taskId,
+        startTime: startTime,
+        newTaskId: newTaskId,
+        newComment: newComment,
+        newStartTime: newStartTime,
+        newEndTime: newEndTime });
+    
+    return jsonData;
+}
+
+function convertDeleteTaskToJson(date, taskId, startTime) {
+    var currentYear = date.getFullYear();
+    var currentMonth = date.getMonth() + 1;
+    var currentDay = date.getDate();
+    
+    var jsonData = JSON.stringify({ year: currentYear, 
+        month: currentMonth, 
+        day: currentDay,
+        taskId: taskId,
+        startTime: startTime });
+    
+    return jsonData;
+}
+
+function postTaskToBackend(date, taskId, comment, startTime, endTime) {
+    var startTaskJson = convertStartTaskToJson(date, taskId, comment, startTime);
+    var finishTaskJson = convertFinishTaskToJson(date, taskId, startTime, endTime);
+    backendPost("/timelogger/workmonths/workdays/tasks/start", startTaskJson);
+    backendPut("/timelogger/workmonths/workdays/tasks/finish", finishTaskJson);
+}
+
+function putModifyTaskToBackend(date, taskId, startTime, newTaskId, 
+                                newComment, newStartTime, newEndTime) {
+    var modifyTaskJson = convertModifyTaskToJson(date, taskId, startTime, newTaskId, 
+                                newComment, newStartTime, newEndTime);
+    backendPut("/timelogger/workmonths/workdays/tasks/modify", modifyTaskJson);
+}
+
+function deleteTaskFromBackend(date, taskId, startTime) {
+    var deleteTaskJson = convertDeleteTaskToJson(date, taskId, startTime);
+    backendDelete("/timelogger/workmonths/workdays/tasks/delete", deleteTaskJson);
+}
+
+function getWorkdayFromBackend(date) {
+    var currentYear = date.getFullYear();
+    var currentMonth = date.getMonth() + 1;
+    var endpoint = "timelogger/workmonths/" + currentYear + "/" + currentMonth;
+
+    var allWorkdays = backendGet(endpoint);
+    
+    var currentDay = date.getDate();
+    for (var i in allWorkdays) {
+        var checkDate = new Date(allWorkdays[i].actualDay);
+        if (checkDate.getDate() === currentDay) {
+            return allWorkdays[i];
+        }
+    }
+    return null;
+}
+
+function backendGet(endpoint){
+    var url = "http://" + window.location.host + "/tlog-backend/" + endpoint;
+
+    var response = null;
+    $.ajax({
+        type : "GET",
+        url : url,
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(result) {
+            response = result;
+        },
+        error : function(e) {
+            handleEndpointException(e);
+        }
+    });
+    return response;
+}
+
+function backendPost(endpoint, json) {
+    var url = "http://" + window.location.host + "/tlog-backend/" + endpoint;
+
+    var response = null;
+    $.ajax({
+        type: "POST",
+        url: url,
+        async: false,
+        data: json,
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            response = result;
+        },
+        error: function(e) {
+            handleEndpointException(e);
+        }
+    });
+    return response;
+}
+
+function backendPut(endpoint, json) {
+    var url = "http://" + window.location.host + "/tlog-backend/" + endpoint;
+
+    var response = null;
+    $.ajax({
+        type: "PUT",
+        url: url,
+        async: false,
+        data: json,
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            response = result;
+        },
+        error: function(e) {
+            handleEndpointException(e);
+        }
+    });
+    return response;
+}
+
+function backendDelete(endpoint, json) {
+    var url = "http://" + window.location.host + "/tlog-backend/" + endpoint;
+
+    var response = null;
+    $.ajax({
+        type: "DELETE",
+        url: url,
+        async: false,
+        data: json,
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            response = result;
+        },
+        error: function(e) {
+            handleEndpointException(e);
+        }
+    });
+    return response;
+}
+
+function handleEndpointException(exception) {
+    var id = parseInt(exception.responseText);
+
+    switch (id) {
+        case WEEKEND_NOT_ENABLED_EXCEPTION:
+            $('#modalConfirmWeekend').modal('toggle');
+            break;
+        case FUTURE_WORK_EXCEPTION:
+            $('#errorInformationText').empty();
+            $('#errorInformationText').append("The chosen day is in the future.<br>");
+            $('#errorInformationText').append("Please choose another day.");
+            $('#modalErrorInformation').modal('toggle');
+            break;
+        default:
+            $('#errorInformationText').empty();
+            $('#errorInformationText').append("Unknown error.<br>");
+            $('#errorInformationText').append("Status code: " + exception.status + "<br>");
+            $('#errorInformationText').append("Error id: " + id);
+            $('#modalErrorInformation').modal('toggle');
+            break;
+    }
+}
+
+const WEEKEND_NOT_ENABLED_EXCEPTION = 1;
+const FUTURE_WORK_EXCEPTION = 2;
